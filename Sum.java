a2f8276ecf3f599dadc44552d3dea18aa87f2cab
Brian672 <54905623+Brian672@users.noreply.github.com>
2019-09-09 12:06:19 -0400

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Sum {
+	public static void main(String [] args) {
+		Scanner input = new Scanner (System.in);
+		System.out.println("Enter 3 Numbers");
+		int num1 = input.nextInt();
+		int num2 = input.nextInt();
+		int num3 = input.nextInt();
+		int result = (num1 + num2 + num3);
+		System.out.println(result);
+	}
+}
