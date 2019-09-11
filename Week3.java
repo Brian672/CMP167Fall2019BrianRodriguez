6eac79755dd6075ea7f9c88eca0d64fbd961c653
Brian672 <54905623+Brian672@users.noreply.github.com>
2019-09-11 12:32:20 -0400

    Add files via upload

---
+++ Week3.java
@@ -0,0 +1,21 @@
+
+public class Week3 { 
+public static void main(String[] args) {
+//	System.out.println(78+98);  //  176
+//	System.out.println('a' + 'b');  // 195
+//	System.out.println('a' - 'b');  // -1
+//	System.out.println('0' + '0');  // 96
+//	System.out.println('a' +1);  // 98
+//	System.out.println('a');  //a
+	
+	int num = 2;
+	double dollars = 15.60;
+	String name1 = "Brian";
+	String name2 = "Justan";
+	
+	System.out.printf(" I have %d dollars", num);// I have 10 cars
+	System.out.printf(" Ronaldy has %.1f in his pocket", dollars);
+	
+	System.out.printf("%n %s is %d inches taller than %s", name1, num, name2);
+	}
+}
