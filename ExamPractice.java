58861c984e4a3dfbd4f71f335f0843ec8f348181
Brian672 <54905623+Brian672@users.noreply.github.com>
2019-10-16 12:29:38 -0400

    Add files via upload

---
+++ ExamPractice.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ExamPractice {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		
+		System.out.println("Enter the abbreviation: ");
+		String abbreviation = scnr.nextLine();
+		
+		String translation = "";
+	public static String Translate(String abbre) {
+			String lol = "Laughing out Loud";
+			String tmi = "Too Much Information";
+			String smh = "Shaking My Head";
+			System.out.println(abbreviation + "means" + translation);
+		}
+			
+			
+		}
+	
+
+	}
+
+
